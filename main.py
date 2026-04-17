868bd678c175ec2d7806e10147e7419172e6630f
M4nvid <manvid.prigrano@gmail.com>
2026-04-17 10:39:26 +0300

    Recover repo after corruption

---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame
+import sys
+from constants import *
+from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from circleshape import CircleShape
+from asteroidfield import AsteroidField
+from logger import log_event
+from shot import Shot
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
+    player = Player(x, y)
+    asteroid_field = AsteroidField()
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player) == True:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+if __name__ == "__main__":
+    main()
